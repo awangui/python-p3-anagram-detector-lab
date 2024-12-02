7e3c75ab6e912cd99f0aa9006730bc77aa905b10
Anita Wangui <anitawangui22@gmail.com>
2024-12-02 17:57:29 +0300

    add anagram check

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def is_anagram(self, word):
+        return sorted(self.word) == sorted(word)
+
+    def match(self, words):
+        return [word for word in words if self.is_anagram(word.lower())]
